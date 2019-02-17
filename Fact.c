2ca9dfc2356edef8d9e7e531522f0693ff78424e
moulikamouli <47565377+moulikamouli@users.noreply.github.com>
2019-02-17 19:29:31 +0530

    Create Fact.c

---
+++ Fact.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int i=1,factorial=1;
+   int n;
+   clrscr();
+   printf("to find factorial of this number\n");
+   printf("enter a number\n");
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+       factorial=factorial*i;
+   }
+   printf("factorial of %d =%d",factorial,n);
+getch();
+}
+
+
